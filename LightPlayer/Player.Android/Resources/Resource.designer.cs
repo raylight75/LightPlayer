011a4d90bda3b1147b867eeb346606a184d00408
raylight75@gmail.com <raylight75@gmail.com>
2019-01-26 14:33:01 +0200

    New Icon

--- LightPlayer/Player.Android/Resources/Resource.designer.cs
+++ LightPlayer/Player.Android/Resources/Resource.designer.cs
@@ -4652,26 +4652,26 @@ namespace Player.Droid
 			// aapt resource value: 0x7f020055
 			public const int avd_hide_password = 2130837589;
 			
+			// aapt resource value: 0x7f02014a
+			public const int avd_hide_password_1 = 2130837834;
+			
 			// aapt resource value: 0x7f02014b
-			public const int avd_hide_password_1 = 2130837835;
+			public const int avd_hide_password_2 = 2130837835;
 			
 			// aapt resource value: 0x7f02014c
-			public const int avd_hide_password_2 = 2130837836;
-			
-			// aapt resource value: 0x7f02014d
-			public const int avd_hide_password_3 = 2130837837;
+			public const int avd_hide_password_3 = 2130837836;
 			
 			// aapt resource value: 0x7f020056
 			public const int avd_show_password = 2130837590;
 			
+			// aapt resource value: 0x7f02014d
+			public const int avd_show_password_1 = 2130837837;
+			
 			// aapt resource value: 0x7f02014e
-			public const int avd_show_password_1 = 2130837838;
+			public const int avd_show_password_2 = 2130837838;
 			
 			// aapt resource value: 0x7f02014f
-			public const int avd_show_password_2 = 2130837839;
-			
-			// aapt resource value: 0x7f020150
-			public const int avd_show_password_3 = 2130837840;
+			public const int avd_show_password_3 = 2130837839;
 			
 			// aapt resource value: 0x7f020057
 			public const int background = 2130837591;
@@ -5375,11 +5375,11 @@ namespace Player.Droid
 			// aapt resource value: 0x7f020140
 			public const int notification_icon_background = 2130837824;
 			
-			// aapt resource value: 0x7f020149
-			public const int notification_template_icon_bg = 2130837833;
+			// aapt resource value: 0x7f020148
+			public const int notification_template_icon_bg = 2130837832;
 			
-			// aapt resource value: 0x7f02014a
-			public const int notification_template_icon_low_bg = 2130837834;
+			// aapt resource value: 0x7f020149
+			public const int notification_template_icon_low_bg = 2130837833;
 			
 			// aapt resource value: 0x7f020141
 			public const int notification_tile_bg = 2130837825;
@@ -5391,19 +5391,16 @@ namespace Player.Droid
 			public const int pixel_full_screen = 2130837827;
 			
 			// aapt resource value: 0x7f020144
-			public const int Play = 2130837828;
+			public const int sc_small = 2130837828;
 			
 			// aapt resource value: 0x7f020145
-			public const int sc_small = 2130837829;
+			public const int splash = 2130837829;
 			
 			// aapt resource value: 0x7f020146
-			public const int splash = 2130837830;
+			public const int tooltip_frame_dark = 2130837830;
 			
 			// aapt resource value: 0x7f020147
-			public const int tooltip_frame_dark = 2130837831;
-			
-			// aapt resource value: 0x7f020148
-			public const int tooltip_frame_light = 2130837832;
+			public const int tooltip_frame_light = 2130837831;
 			
 			static Drawable()
 			{
